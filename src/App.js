19b8e128aaf7cace492ef0e9c7ecc4a205e4c3cc
Shawn Chang <shawnchang@shawnchang-m62.lan>
2022-11-15 15:06:57 -0800

    add about me photo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-import BucketList from './components/BucketList';
+import React from 'react';
+
+import About from './components/About/';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Portfolio from './components/Portfolio';
+import Project from './components/Project';
+import Resume from './components/Resume';
+
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
+    <div>
+      <Header />
+      <Navbar />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Portfolio() {
+    return (
+      <header className="header">
+        <h1>Home</h1>
+      </header>
+    );
+  }
+
+export default Portfolio;
